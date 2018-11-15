f6c5281d74166c6f6d66709faee77115776e52ee
ashishpurey <42497834+ashishpurey@users.noreply.github.com>
2018-11-15 16:52:51 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,17 @@
+package com.art.build.maven.simple;
+
+public class main {
+	public main(String[] args) {
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		
+	}
+
+}
